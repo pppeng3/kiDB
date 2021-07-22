7f8aa077d47f5db3c6ee48d2c1cc30890fd3afaa
Apale7 <1092377056@qq.com>
2021-07-23 01:27:16 +0800

    some definition

--- consts/datatype.go
+++ consts/datatype.go
@@ -9,3 +9,8 @@ const (
 	HashSet
 	ZSet
 )
+
+// Serializable Each datastructure has to implement this interface for serializing to hard disk
+type Serializable interface {
+	Serialize() []byte
+}
--- consts/operation.go
+++ consts/operation.go
@@ -3,5 +3,20 @@ package consts
 type Operation uint8
 
 const (
-	Set Operation = iota
+	insert Operation = iota
 )
+
+// Command 所有对数据的操作都会抽象成command
+// 例如: set name apale会抽象成插入一条Command{OperationType: insert, key: name, value: apale}
+// 而del name会抽象成Command{OperationType: insert, key: name, value: apale}.
+// 这样设计是为了实现LSM Tree
+// All kinds of operations for data would be abstracted as a command.
+// For example, set name apale would be insert as a Command{OperationType: insert, key: name, value: apale}.
+// While del name would be insert as Command{OperationType: delete, key: name, value: }.
+// This is designed for LSM Tree
+type Command struct {
+	OperationType Operation
+	ValueType     DataType
+	Key           []byte
+	Value         []byte
+}
